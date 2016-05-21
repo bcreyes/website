1e09922f5f79b9346148a0cb9b2655779d71a78a
bjorn <bjorn>
2016-05-21 13:15:34 -0500

    addes changes to index.html and myApp.js

--- public/myApp.js
+++ public/myApp.js
@@ -4,6 +4,13 @@ app.controller("textArea", function($scope){
   $scope.text = "This is a very long paragraph" +
   "to show the extent of a text area" +
   "and demostrate scope in angular controllers."
+
+  $scope.first_name = "Bjorn";
+  $scope.last_name = "Reyes";
+  $scope.portrait = "images/portrait.jpg";
+  $scope.full_name = function(){
+    return $scope.first_name + " " + $scope.last_name;
+  }
   $scope.textClick = function(){
     $scope.text = "I just change the text";
   }
